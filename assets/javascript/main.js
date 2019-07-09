dacb31748bc5704d2417a7e658f70e154544f580
TheResinger <49324943+TheResinger@users.noreply.github.com>
2019-07-08 23:58:04 -0400

    Add files via upload

---
+++ assets/javascript/main.js
@@ -0,0 +1,38 @@
+$(document).ready(function(){
+
+});
+
+var game = {
+    // Set variables
+    right : 0, //Answers Right
+    wrong : 0, //Answers Wrong
+    clockTime : 20,  // 20 Second timer
+    questions : {
+        1 : "Question 1",
+        2 : "Question 2",
+        3 : "Question 3",
+        4 : "Question 4",
+        5 : "Question 5",
+        6 : "Question 6",
+        7 : "Question 7",
+    },
+    answers : {
+        1 : ["test", "test", "test", "test"],
+        2 : ["test", "test", "test", "test"],
+        3 : ["test", "test", "test", "test"],
+        4 : ["test", "test", "test", "test"],
+        5 : ["test", "test", "test", "test"],
+        6 : ["test", "test", "test", "test"],
+        7 : ["test", "test", "test", "test"],
+    },
+    rightAnswers : {
+        1 : "test",
+        2 : "test",
+        3 : "test",
+        4 : "test",
+        5 : "test",
+        6 : "test",
+        7 : "test",
+    },
+}
+
