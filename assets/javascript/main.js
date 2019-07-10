0fc2610eb6a50efd9be5580365016e6f5e9dcb66
TheResinger <49324943+TheResinger@users.noreply.github.com>
2019-07-10 16:10:45 -0400

    Add files via upload

--- assets/javascript/main.js
+++ assets/javascript/main.js
@@ -1,38 +1,79 @@
 $(document).ready(function(){
-
+    $("#mainContainer").append($("<h1>",{"text" : "Welcome to Trivia!", "id" : "welcome", "class" : "center"}));
+    $("#mainContainer").append($("<button>", {"type" : "button", "id" : "start", "text" : "Click to Start", "class" : "center"}));
+    $("#start").on("click", game.start);
+    $(document).on("click", ".option", game.checkUserGuess);
 });
 
 var game = {
     // Set variables
     right : 0, //Answers Right
     wrong : 0, //Answers Wrong
-    clockTime : 20,  // 20 Second timer
+    currentQ : 0,
+    clockTime : 10,  // 10 Second timer
+    clockRunning : false,
+    clock : '',
     questions : {
-        1 : "Question 1",
-        2 : "Question 2",
-        3 : "Question 3",
-        4 : "Question 4",
-        5 : "Question 5",
-        6 : "Question 6",
-        7 : "Question 7",
+        1 : "Which has NOT been a Nissan slogan?",
+        2 : "What is the luxury division of Nissan? ",
+        3 : "What is the motorsports and performance division of Nissan?",
+        4 : "What was the first year of production for the Nissan 350Z?",
     },
     answers : {
-        1 : ["test", "test", "test", "test"],
-        2 : ["test", "test", "test", "test"],
-        3 : ["test", "test", "test", "test"],
-        4 : ["test", "test", "test", "test"],
-        5 : ["test", "test", "test", "test"],
-        6 : ["test", "test", "test", "test"],
-        7 : ["test", "test", "test", "test"],
+        1 : ["Life is a journey. Enjoy the ride.", "Moving Forward.", "Everything you want, nothing you don’t.", "Built for the Human Race."],
+        2 : ["Infiniti", "Lexus", "Audi", "Acura"],
+        3 : ["NISMO", "None of these", "NISSPORT", "NMCRT"],
+        4 : ["2004", "2000", "2006", "2002"],
     },
     rightAnswers : {
-        1 : "test",
-        2 : "test",
-        3 : "test",
-        4 : "test",
-        5 : "test",
-        6 : "test",
-        7 : "test",
+        1 : "Moving Forward.",
+        2 : "Infiniti",
+        3 : "NISMO",
+        4 : "2002",
     },
+    start : function(){
+        game.currentQ = 0;
+        game.right = 0;
+        game.wrong = 0;
+        $("#start").remove();
+        $("#welcome").remove();
+        $("#mainContainer").append($("<div>",{"id" : "game"}));
+        $("#game").append($("<h3>",{"text" : "Time Left : " + game.clockTime, "class" : "center"}));
+        game.next();
+    },
+    next :  function(){
+        game.clockTime = 20;
+        var questionList = Object.values(game.questions)[game.currentQ];
+        $("#game").append($("<div>",{"id" : "qna", "class" : "row"}));
+        $("#qna").append($("<h1>",{"text" : questionList, "id" : "Question", "class" : "col s12 center"}));
+        $("#qna").append($("<h1>",{"id" : "result", "class" : "col s12 center"}));
+        var questionAnswers = Object.values(game.answers)[game.currentQ];
+        questionAnswers.forEach(function(index, key){
+            $("#qna").append($("<h4>", {"text" : index, "id" : key, "class" : "col s3 center option"}));
+        });
+    },
+    checkUserGuess : function(){
+        var correctAnswer = Object.values(game.rightAnswers)[game.currentQ];
+        var resultID;
+        if ($(this).text() === correctAnswer)
+        {
+            console.log("correct");
+            game.right++;
+            resultID = setTimeout(game.result, 5000);
+            $("#result").text("Correct!");
+        }
+        else
+        {
+            console.log("wrong");
+        }
+    },
+    result : function(){
+        $("#qna").remove();
+        game.currentQ++;
+        game.next();
+    },
+    clockRunning : function(){
+
+    }
 }
 
